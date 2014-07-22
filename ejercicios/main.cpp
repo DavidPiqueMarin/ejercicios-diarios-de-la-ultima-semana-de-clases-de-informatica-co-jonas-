b25b9ff004dc477c1a6c230e7fc088caa5084e1b
DavidPiqueMarin <macbook@MacBook-de-MacBook.local>
2014-07-22 12:02:14 +0100

    commit inicial

---
+++ ejercicios/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  ejercicios
+//
+//  Created by maxilicamtropo-4 on 22/07/14.
+//  Copyright (c) 2014 maxilicamtrop-4. All rights reserved.
+//
+
+#include <iostream>
+
+int main()
+{
+    //tarea martes 22 de julio
+    
+    return 0;
+}
+
