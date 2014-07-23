ed52555b8cebe22f216e74e5a35786546c4fbe38
DavidPiqueMarin <macbook@MacBook-de-MacBook.local>
2014-07-23 12:23:42 +0100

    ejercicio hecho

--- ejercicios/main.cpp
+++ ejercicios/main.cpp
@@ -7,11 +7,26 @@
 //
 
 #include <iostream>
+using namespace std;
+
 
 int main()
 {
     //tarea martes 22 de julio
+    cout << "1. mostrar mi nombre" << endl;
+    cout << "2. Mostrar mi apellido" << endl;
+    int opcion;
+    cin >> opcion;
+    if (opcion==1) {
+        cout << "jonas"<< endl;
     
+    }
+    if (opcion==2){
+        cout << "lopez mesa"<< endl;
+    }
     return 0;
 }
 
+
+    
+    
