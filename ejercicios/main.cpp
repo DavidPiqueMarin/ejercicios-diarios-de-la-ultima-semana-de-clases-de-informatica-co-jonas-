2b6e1e7b61949acdd5e2760e3396f767d67598c0
DavidPiqueMarin <macbook@MacBook-de-MacBook.local>
2014-07-25 11:43:54 +0100

    ejercicio resuelto

--- ejercicios/main.cpp
+++ ejercicios/main.cpp
@@ -13,7 +13,7 @@ using namespace std;
 int main()
 {
     //tarea martes 22 de julio
-    cout << "1. mostrar mi nombre" << endl;
+    /*cout << "1. mostrar mi nombre" << endl;
     cout << "2. Mostrar mi apellido" << endl;
     int opcion;
     cin >> opcion;
@@ -23,7 +23,21 @@ int main()
     }
     if (opcion==2){
         cout << "lopez mesa"<< endl;
+    }*/
+    while (1==1) {
+        cout << "introduzca el numero" << endl;
+        int introduzca;
+        cin >> introduzca;
+        if (introduzca >= 5) {
+            cout << "el numero es mayor que 5"<< endl ;
+        }
+        if (introduzca <=4) {
+            cout << "el numero es menor que 5"<< endl ;
+        }
     }
+    
+
+    
     return 0;
 }
 
